300398dab40060c29983adc5042f49ff422b5978
Ryan <rkirmis@asu.edu>
2019-02-16 15:06:41 -0700

    Bug-10: fixed public class-member variable by changing it to private and adding a getter and setter.

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -1,10 +1,34 @@
 package main.java;
 
 public final class Item {
-        public int key;
+        private int key; // SER316 Assignment #4 - made variable private and added getter and setter for it
         //constructor
         public Item(int keyValue) { 
                 key = keyValue;
         }
         // further components and methods
+        
+        /**
+         * Method: getKey
+         * Inputs: none
+         * Returns: key - the Item's key
+         * 
+         * Description: Gets the key of the Item.
+         */
+        	public int getKey()
+        	{
+        		return key;
+        	}
+        	
+        	/**
+         * Method: setKey
+         * Inputs: key - the item's new key
+         * Returns: none
+         * 
+         * Description: Sets the key of the Item.
+         */
+        	public void setKey(int key)
+        	{
+        		this.key = key;
+        	}
 }
